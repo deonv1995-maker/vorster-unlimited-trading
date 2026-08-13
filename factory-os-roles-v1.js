a7e1a3fcb9ca6ef20f2089f65ce8c73e326599bf
deonv1995-maker <deonv1995@gmail.com>
2026-08-13 20:13:16 +0200

    Add Factory OS role definitions

---
+++ factory-os-roles-v1.js
@@ -0,0 +1,12 @@
+(function(){
+'use strict';
+window.VUFactoryOSRoles={
+ Management:['Manufacturing','Finishing & Painting','Orders','Delivery Schedule','Collection Schedule','Stock','Planning'],
+ Office:['Import / Update Orders','Order Status','Delivery Schedule','Collection Schedule'],
+ Casting:['Today’s Casting','Casting Stock'],
+ Packing:['Today’s Packing','Packing Stock'],
+ Resin:['Today’s Resin','Resin Stock'],
+ Painting:['Finishing & Painting','Finished Stock'],
+ Delivery:['Today’s Deliveries','Collections']
+};
+})();
